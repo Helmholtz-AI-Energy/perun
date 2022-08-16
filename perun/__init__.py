3dbd7b3a46d6cfed2627851b6e3c4cb9d6cfcbad
Gutiérrez Hermosillo Muriedas, Juan Pedro <juanpedroghm@gmail.com>
2022-08-16 17:13:52 +0200

    fix: isort ignores perun/__init__.py

--- perun/__init__.py
+++ perun/__init__.py
@@ -6,17 +6,18 @@ import os
 
 from dotenv import load_dotenv
 
+# SETUP ENV
+load_dotenv()
+os.environ["IBV_FORK_SAFE"] = "1"
+os.environ["RDMAV_FORK_SAFE"] = "1"
+log_lvl = os.environ["LOG_LVL"] if "LOG_LVL" in os.environ else "INFO"
+
 from perun.backend import Device, backends
 from perun.logging import init_logging
 from perun.perun import getDeviceConfiguration, monitor, perunSubprocess, postprocessing
 from perun.report import report
 from perun.storage import ExperimentStorage, LocalStorage
 
-# SETUP ENV
-load_dotenv()
-os.environ["IBV_FORK_SAFE"] = "1"
-os.environ["RDMAV_FORK_SAFE"] = "1"
-log_lvl = os.environ["LOG_LVL"] if "LOG_LVL" in os.environ else "INFO"
 log = init_logging(log_lvl)
 
 
@@ -30,4 +31,5 @@ __all__ = [
     "log",
     "monitor",
     "report",
+    "postprocessing",
 ]
