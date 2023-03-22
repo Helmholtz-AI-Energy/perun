fda408e098f78011e690a7817b4d474b2358e3cc
Gutiérrez Hermosillo Muriedas, Juan Pedro <juanpedroghm@gmail.com>
2023-03-22 17:55:58 +0100

    feat: summary with co2 and price in text report

--- perun/configuration.py
+++ perun/configuration.py
@@ -11,8 +11,8 @@ import click
 _default_config: Mapping[str, Mapping[str, Any]] = {
     "post-processing": {
         "pue": 1.58,
-        "emissions_factor": 0.355,
-        "price_factor": 41.59,
+        "emissions_factor": 0.262,  # gCO2eq/kWh
+        "price_factor": 34.60,  # Cent/kWh
     },
     "monitor": {
         "sampling_rate": 1,
