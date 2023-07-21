ab2335f48d6de965aba19924500e5398455d804d
JuanPedroGHM <juanpedroghm@gmail.com>
2023-07-21 15:14:23 +0200

    fix: nvidia-ml-py compatibility
    * 0.3.2
    
    0.3.2 [skip ci]
    
    * fix: official nvml bidings
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: io3047@kit.edu <io3047@uc2n520.localdomain>
    Co-authored-by: io3047@kit.edu <io3047@uc2n994.localdomain>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- perun/__init__.py
+++ perun/__init__.py
@@ -1,6 +1,6 @@
 """perun module."""
 # flake8: noqa
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 from perun.configuration import config
 from perun.logging import init_logging
 
@@ -8,6 +8,7 @@ log = init_logging(config.get("debug", "log_lvl"))
 
 import os
 
+os.environ["OMPI_MCA_mpi_warn_on_fork"] = "0"
 os.environ["IBV_FORK_SAFE"] = "1"
 os.environ["RDMAV_FORK_SAFE"] = "1"
 
