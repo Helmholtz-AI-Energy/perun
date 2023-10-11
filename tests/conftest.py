6737b9efc8147f71995cc4124464edd7c53c518e
Gutiérrez Hermosillo Muriedas, Juan Pedro <juanpedroghm@gmail.com>
2023-10-11 20:46:15 +0200

    ci: more showconf unit test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,19 @@
 # noqa
+import configparser
+
 import pytest
 
-from perun import config
+from perun.configuration import _default_config
 from perun.perun import Perun
 
 
 @pytest.fixture(scope="package")
-def perun():
-    return Perun(config)
+def defaultConfig():
+    defaultConfig = configparser.ConfigParser(allow_no_value=True)
+    defaultConfig.read_dict(_default_config)
+    return defaultConfig
+
+
+@pytest.fixture(scope="package")
+def perun(defaultConfig):
+    return Perun(defaultConfig)
--- tests/perun/api/test_cli.py
+++ tests/perun/api/test_cli.py
@@ -1,5 +1,6 @@
 import configparser
 import subprocess
+from pathlib import Path
 
 from perun.api.cli import _get_arg_parser
 from perun.perun import Perun
@@ -22,17 +23,84 @@ def test_sensors_command(perun: Perun):
     assert processOut == expectedResult
 
 
-def test_showconf_command(perun: Perun):
+def test_showconf_command(defaultConfig: configparser.ConfigParser):
     # 1) Are the outputs the same?
     processorOut = subprocess.run(
         ["perun", "showconf"], capture_output=True, text=True
     ).stdout
     parser = configparser.ConfigParser(allow_no_value=True)
     parser.read_string(processorOut)
-    assert parser == perun.config
+    assert parser == defaultConfig
 
+
+def test_showconf_command_with_cli_args(defaultConfig: configparser.ConfigParser):
     # 2) Are cli arguments correctly set?
+    processorOut = subprocess.run(
+        ["perun", "--log_lvl", "ERROR", "showconf"], capture_output=True, text=True
+    ).stdout
+    parser = configparser.ConfigParser(allow_no_value=True)
+    parser.read_string(processorOut)
+    defaultConfig.set("debug", "log_lvl", "ERROR")
+    assert parser == defaultConfig
 
+
+def test_showconf_command_with_conf_file(
+    defaultConfig: configparser.ConfigParser, tmp_path: Path
+):
     # 3) Are files read correctly?
+    confPath = tmp_path / ".perun.ini"
+    defaultConfig.set("monitor", "sampling_rate", "2")
+    with open(confPath, "w+") as configFile:
+        defaultConfig.write(configFile)
 
+    processorOut = subprocess.run(
+        ["perun", "--configuration", str(confPath), "showconf"],
+        capture_output=True,
+        text=True,
+    ).stdout
+    parser = configparser.ConfigParser(allow_no_value=True)
+    parser.read_string(processorOut)
+    assert parser.get("monitor", "sampling_rate") == "2"
+    assert parser == defaultConfig
+
+
+def test_showconf_command_with_default(
+    defaultConfig: configparser.ConfigParser, tmp_path: Path
+):
     # 4) Does default ignore everything?
+    confPath = tmp_path / ".perun.ini"
+    defaultConfig.set("monitor", "sampling_rate", "2")
+    with open(confPath, "w+") as configFile:
+        defaultConfig.write(configFile)
+
+    processorOut = subprocess.run(
+        ["perun", "--log_lvl", "INFO", "--configuration", str(confPath), "showconf"],
+        capture_output=True,
+        text=True,
+    ).stdout
+    parser = configparser.ConfigParser(allow_no_value=True)
+    parser.read_string(processorOut)
+    assert parser.get("debug", "log_lvl") == "INFO"
+    assert parser.get("monitor", "sampling_rate") == "2"
+    assert parser != defaultConfig
+
+    defaultConfig.set("monitor", "sampling_rate", "1")
+    processorOut = subprocess.run(
+        [
+            "perun",
+            "--log_lvl",
+            "INFO",
+            "--configuration",
+            str(confPath),
+            "showconf",
+            "--default",
+        ],
+        capture_output=True,
+        text=True,
+    ).stdout
+    parser = configparser.ConfigParser(allow_no_value=True)
+    parser.read_string(processorOut)
+
+    assert parser.get("debug", "log_lvl") == "WARNING"
+    assert parser.get("monitor", "sampling_rate") == "1"
+    assert parser != defaultConfig
