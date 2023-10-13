6fa82bc55873b4341ff5da1834a8221530404da9
Gutiérrez Hermosillo Muriedas, Juan Pedro <juanpedroghm@gmail.com>
2023-10-13 11:06:12 +0200

    ci: monitor, export and metadata cli tests

--- tests/perun/api/test_cli.py
+++ tests/perun/api/test_cli.py
@@ -1,6 +1,9 @@
 import configparser
+import json
 import subprocess
 from pathlib import Path
+
+import pytest
 
 from perun.api.cli import _get_arg_parser
 from perun.perun import Perun
@@ -104,3 +107,78 @@ def test_showconf_command_with_default(
     assert parser.get("debug", "log_lvl") == "WARNING"
     assert parser.get("monitor", "sampling_rate") == "1"
     assert parser != defaultConfig
+
+
+def test_metadata_command(perun: Perun):
+    processorOut = subprocess.run(
+        ["perun", "metadata"], capture_output=True, text=True, timeout=10
+    ).stdout
+    metadataJson = json.loads(processorOut)
+    for host in perun.host_rank.keys():
+        assert host in metadataJson
+
+
+def test_monitor_command(tmp_path: Path):
+    # Test Monitor
+    testFilePath = tmp_path / "idle.py"
+    with open(testFilePath, "w+") as testFile:
+        testFile.write("import time\n\ntime.sleep(10)")
+
+    resultsPath = tmp_path / "results"
+
+    subprocess.run(
+        f"perun monitor --data_out {resultsPath} {testFilePath}".split(" "), timeout=20
+    )
+
+    # Expected files, hdf5 file and a text file with a date
+    # Are the files in the correct folder
+    resultFiles = list(resultsPath.iterdir())
+    assert len(resultFiles) == 2
+    assert resultsPath / "idle.hdf5" in resultFiles
+    assert (resultsPath / "idle.hdf5").is_file()
+
+    resultFiles.remove(resultsPath / "idle.hdf5")
+    textFile = resultFiles.pop()
+    assert textFile.is_file()
+    assert textFile.suffix == ".txt"
+
+
+@pytest.mark.parametrize(
+    "format,suffix", [("json", "json"), ("csv", "csv"), ("pickle", "pkl")]
+)
+def test_export_command(format: str, suffix: str, tmp_path: Path):
+    testFilePath = tmp_path / "idle.py"
+    with open(testFilePath, "w+") as testFile:
+        testFile.write("import time\n\ntime.sleep(10)")
+
+    resultsPath = tmp_path / "results"
+    subprocess.run(
+        f"perun monitor --data_out {resultsPath} {testFilePath}".split(" "), timeout=20
+    )
+
+    # Expected files, hdf5 file and a text file, and a csv file
+    # Are the files in the correct folder
+    resultFiles = list(resultsPath.iterdir())
+    assert len(resultFiles) == 2
+    assert resultsPath / "idle.hdf5" in resultFiles
+
+    resultFiles.remove(resultsPath / "idle.hdf5")
+    textFile = resultFiles.pop()
+    assert textFile.is_file()
+    assert textFile.suffix == ".txt"
+
+    # Test export
+    subprocess.run(
+        f"perun export {resultsPath / 'idle.hdf5'} {format}".split(" "), timeout=10
+    )
+    resultFiles = list(resultsPath.iterdir())
+    assert len(resultFiles) == 3
+    assert resultsPath / "idle.hdf5" in resultFiles
+    assert textFile in resultFiles
+
+    resultFiles.remove(resultsPath / "idle.hdf5")
+    resultFiles.remove(textFile)
+
+    exportedFile = resultFiles.pop()
+    assert exportedFile.is_file()
+    assert exportedFile.suffix == f".{suffix}"
