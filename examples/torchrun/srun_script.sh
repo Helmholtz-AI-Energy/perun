28fc0f78c3e1d17a9383a8ef649fc15a6e44062d
Gutiérrez Hermosillo Muriedas, Juan Pedro <juanpedroghm@gmail.com>
2025-08-26 16:44:48 +0200

    feat: single node example for torchrun

---
+++ examples/torchrun/srun_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Load environment
+source .venv/bin/activate
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+export MASTER_PORT=$(expr 2950 + $(echo -n $SLURM_JOBID | tail -c 4))
+export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export HSA_FORCE_FINE_GRAIN_PCIE=1
+
+
+srun bash -lc 'torchrun \
+                --nnodes=$SLURM_JOB_NUM_NODES --nproc-per-node gpu \
+                --rdzv_backend=c10d --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT \
+                 ddp.py'
